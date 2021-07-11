f34a9b9cf174382402f50baf43e0b939aff9202b
Shubham Gote <0shubhamgote0@gmail.com>
2021-07-11 18:46:33 +0530

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,8 +1,3 @@
 */var obj = JSON.parse($response.body); 
 obj['pro_plan'] = pro_premium;
-"badges" : [
-    {
-      "name" : "pro:pro_premium",
-      "verbose_name" : "Premium"
-    }
 $done({body: JSON.stringify(obj)});
