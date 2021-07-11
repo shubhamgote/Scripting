76cde6ee1c10556f1934b4507944ef93f1b40a4d
Shubham Gote <0shubhamgote0@gmail.com>
2021-07-11 16:41:20 +0530

    Update z.js

--- Loon/z.js
+++ Loon/z.js
@@ -1,3 +1,7 @@
-var obj = JSON.parse($response.body); 
-obj['is_premium'] = true;
+var obj = JSON.parse($response.body);
+
+obj["application"]= {
+  "is_premium": true,
+};
+
 $done({body: JSON.stringify(obj)});
