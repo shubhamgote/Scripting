47fafc829e8fe0aa73477cba0758b7abfa35847c
Shubham Gote <0shubhamgote0@gmail.com>
2021-07-11 19:00:02 +0530

    Add files via upload

---
+++ sgt.js
@@ -0,0 +1,42 @@
+{
+  "socials" : {
+    "twitter" : ""
+  },
+  "location" : "",
+  "pro_plan" : "pro_premium_trial",
+  "status" : "",
+  "following_count" : 0,
+  "follow_status" : false,
+  "is_online" : true,
+  "avatars" : {
+    "mid" : "https://s3.tradingview.com/userpics/7317873-eE07_mid.png",
+    "big" : "https://s3.tradingview.com/userpics/7317873-eE07_big.png",
+    "small" : "https://s3.tradingview.com/userpics/7317873-eE07.png",
+    "orig" : "https://s3.tradingview.com/userpics/7317873-eE07_orig.png"
+  },
+  "badges" : [
+    {
+      "name" : "pro:pro_premium_trial",
+      "verbose_name" : "Premium Trial"
+    }
+
+  ],
+  "youtube_channel" : "",
+  "reputation" : 0,
+  "last_visit" : 1626009614,
+  "broker_plan" : null,
+  "is_broker" : false,
+  "id" : 7317873,
+  "website" : "",
+  "followers_count" : 2,
+  "last_locale" : "in",
+  "youtube_channel_name" : "",
+  "publications" : {
+    "ideas" : 0,
+    "scripts" : 0
+  },
+  "is_trial_available" : false,
+  "theme" : "light",
+  "has_phone" : true,
+  "username" : "shubhamg09"
+}
